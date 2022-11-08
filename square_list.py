f1f34d6baa315f0e7811bbbdcb01c9d4d09e4030
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-11-07 21:09:00 -0800

    Update and rename main.py to square_list.py

--- square_list.py
+++ square_list.py
@@ -5,6 +5,9 @@
 
 
 def square_list(num):
+    ' ' '
+    Prints the squares of numbers in an array passed as a parameter
+    ' ' '
     for i in range(0,len(num)):
         num[i]=(num[i])**2
     return num
